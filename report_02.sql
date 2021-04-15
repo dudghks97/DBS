99e2eff635f8290d6d90fe1bcc2559a75717a951
dudghks97 <yh9638@skuniv.ac.kr>
2021-04-16 02:41:20 +0900

    21.04.16 02:41

--- report_02.sql
+++ report_02.sql
@@ -8,18 +8,11 @@ from student as S1, student as S2
 where (S1.tot_cred > S2.tot_cred) and (S2.dept_name = 'Comp. Sci.')
 
 -- 2. Comp. Sci. 학과에 소속된 어떤 학생보다 많은 학점을 이수한 학생들 가운데 Comp. Sci. 학과에 소속되지 않은 학생들의 수를 학과별로 검색.
-select distinct S1.name, S1.dept_name, S1.tot_cred
+select S1.dept_name, count(distinct S1.ID) as student_cnt
 from student as S1, student as S2
 where (S1.tot_cred > S2.tot_cred) and (S2.dept_name = 'Comp. Sci.')
-
-
-select distinct S1.name, S1.dept_name, S1.tot_cred
-from student as S1, student as S2
-where (S1.tot_cred > S2.tot_cred) and (S2.dept_name = 'Comp. Sci.')
-except
-select name, dept_name, tot_cred
-from student
-where dept_name = 'Comp. Sci.'
+group by S1.dept_name
+having S1.dept_name != 'Comp. Sci.'
 
 -- 3. Accounting 학과에 소속된 학생들 중 이름이 Ch 로 시작하는 학생들에 대한 모든 정보를 검색.
 select *
@@ -33,15 +26,6 @@ where budget between 600000 and 800000
 order by budget desc
 
 -- 5. 2006학년도 봄학기에는 강의를 맡았지만 2006학년도 가을학기에는 강의를 맡지 않은 교수들의 교수번호를 검색.
-select *
-from teaches
-where semester = 'Spring' and year = 2006
-
-select *
-from teaches
-where semester = 'Fall' and year = 2009
-
-
 select ID
 from teaches
 where semester = 'Spring' and year = 2006
@@ -59,30 +43,36 @@ group by dept_name
 -- 7. 학년도, 학기 기준으로 수강신청하여 이수한 학생 수를 검색
 -- (수강 신청 후 아직 이수완료하지 않은 경우 grade는 null을 가짐. grade 가 'F'인 경우 해당 강좌를 이수하지 않은 것이 됨.)
 -- 이거 시발 왜 F 받은새끼가 없어
-select ID, year, semester, grade
+select count(distinct ID) as std_cnt, year, semester
 from takes
-group by ID, year, semester, grade
-order by grade desc
+where (grade is not null) and (grade != 'F')
+group by year, semester
+order by year, semester desc
 
-select grade
-from takes
-
--- 8. 학과별 지도학생 수를 검색 -> 맞는지 아닌지 모름
-select instructor.dept_name, count(s_id) as s_cnt
+-- 8. 학과별 지도학생 수를 검색
+select instructor.dept_name, count(s_id) as std_cnt
 from advisor, instructor
 where i_id = ID
 group by instructor.dept_name
 
-select dept_name, count(*) as cnt
-from student
-group by dept_name
-order by dept_name
-
--- 9. 50명 이상 학생들을 지도하는 교수들의 교수번호와 지도학생 수를 검색. -> 맞는지 아닌지 모름
+-- 9. 50명 이상 학생들을 지도하는 교수들의 교수번호와 지도학생 수를 검색
 select i_id, count(s_id) as s_cnt
 from advisor, instructor
 where i_id = ID
 group by i_id
 having count(s_id) >= 50
 
--- 10. 학년도, 강의실(building, room_number) 기준으로 해당 학년도, 강의실에서 진행된 강좌에 참여한 학생 수를 검색.
+-- 10. 학년도, 강의실(building, room_number) 기준으로 해당 학년도, 강의실에서 진행된 강좌에 참여한 학생 수를 검색. --> 미완
+select * from section order by course_id
+select * from takes order by course_id
+
+select takes.year, building, room_number, count(ID) as std_cnt, takes.course_id, takes.sec_id
+from section, takes
+where (takes.course_id = section.course_id) and ()
+group by takes.year, building, room_number, takes.course_id, takes.sec_id
+order by takes.course_id
+
+select course_id, year, semester, sec_id, count(ID) as std_cnt
+from takes
+group by course_id, year, semester, sec_id
+order by course_id
